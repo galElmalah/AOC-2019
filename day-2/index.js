bb945d2eb1ca9bbc52f4e8ee15a4ab27890b5f65
Gal Elmalah <gale@wix.com>
2020-07-16 23:43:25 +0300

    second day is done

---
+++ day-2/index.js
@@ -0,0 +1,42 @@
+const { dataLinesToArrayOfNumbers } = require("../helpers");
+const data = dataLinesToArrayOfNumbers(`${__dirname}/data.txt`, ",");
+
+const opcodes = {
+  "1": (i, input) =>
+    (input[input[i + 2]] = input[input[i]] + input[input[i + 1]]),
+  "2": (i, input) =>
+    (input[input[i + 2]] = input[input[i]] * input[input[i + 1]]),
+};
+
+const firstTask = (input, noun, verb) => {
+  input[1] = noun;
+  input[2] = verb;
+  for (let i = 0; i < input.length; i++) {
+    const currentOp = input[i];
+
+    if (opcodes[currentOp]) {
+      opcodes[currentOp](i + 1, input);
+      i += 3;
+      continue;
+    }
+
+    if (currentOp === 99) {
+      return input[0];
+    }
+  }
+  return input[0];
+};
+
+const secondTask = (input) => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      if (firstTask([...input], i, j) === 19690720) {
+        return 100 * i + j;
+      }
+    }
+  }
+  return "day-2 second task";
+};
+
+console.log("First problem:", firstTask([...data], 12, 2));
+console.log("Second problem:", secondTask([...data]));
--- helpers.js
+++ helpers.js
@@ -3,6 +3,6 @@ const fs = require("fs");
 const readData = (path, enc = "utf-8") => fs.readFileSync(path, enc);
 
 module.exports.dataLinesToArrayOfNumbers = (path, separator = "\n") =>
-  readData(path, "ascii").split(separator);
+  readData(path, "ascii").split(separator).map(Number);
 
 module.exports.readData = (path) => fs.readFileSync(path, "utf-8");
